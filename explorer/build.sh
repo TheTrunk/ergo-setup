990fbb5e121216d6f6d58ef1683aa64f4c05fe4f
TheTrunk <tadeas@runonflux.io>
2026-01-26 23:51:31 +0800

    fix

--- explorer/build.sh
+++ explorer/build.sh
@@ -26,4 +26,44 @@ sed -i.bak 's|v3.3.8-aaaab5ef-SNAPSHOT|3.3.7|g' explorer-backend-${EXPLORER_BRAN
 sed -i.bak 's|4.0.6-31-e2e0ffa1-SNAPSHOT|4.0.7|g' explorer-backend-${EXPLORER_BRANCH}/project/dependencies.scala
 rm -f explorer-backend-${EXPLORER_BRANCH}/project/*.bak
 
+echo "Fixing incompatible PrettyPrintErgoTree API"
+cat > explorer-backend-${EXPLORER_BRANCH}/modules/explorer-api/src/main/scala/org/ergoplatform/explorer/http/api/v1/models/PrettyErgoTree.scala << 'PATCHEOF'
+package org.ergoplatform.explorer.http.api.v1.models
+
+import org.ergoplatform.explorer.HexString
+import sigmastate.lang.exceptions.SerializerException
+import sigmastate.serialization.ErgoTreeSerializer.DefaultSerializer
+
+object PrettyErgoTree {
+  def fromString(s: String) : Either[PrettyErgoTreeError, ErgoTreeHuman] = {
+    HexString.fromString[Either[Throwable, *]](s) match {
+      case Left(_) => Left(PrettyErgoTreeError.BadEncoding)
+      case Right(hexString) => fromHexString(hexString)
+    }
+  }
+
+  def fromHexString(h: HexString): Either[PrettyErgoTreeError, ErgoTreeHuman] = {
+    try {
+      val ergoTree = DefaultSerializer.deserializeErgoTree(h.bytes)
+      ergoTree.root match {
+        case Left(_) => Left(PrettyErgoTreeError.UnparsedErgoTree)
+        case Right(value) =>
+          val script = value.toString
+          val constants = ergoTree.constants.zipWithIndex.map { case (c, i) => s"$i: ${c.value}" }.mkString("\n")
+          Right(ErgoTreeHuman(constants, script))
+      }
+    } catch {
+      case se: SerializerException => Left(PrettyErgoTreeError.DeserializeException(se.message))
+    }
+  }
+}
+
+sealed trait PrettyErgoTreeError
+object PrettyErgoTreeError {
+  case object BadEncoding extends PrettyErgoTreeError
+  case class DeserializeException(msg: String) extends PrettyErgoTreeError
+  case object UnparsedErgoTree extends PrettyErgoTreeError
+}
+PATCHEOF
+
 echo "Done."
