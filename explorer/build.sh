a88673fc6f4f45f8a7e477f11aa2cfcd2747e3cf
TheTrunk <tadeas@runonflux.io>
2026-01-26 23:21:29 +0800

    update

--- explorer/build.sh
+++ explorer/build.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 set -e
 
-EXPLORER_VERSION=9.17.9
+EXPLORER_BRANCH=master
 
-
-echo "Fetching explorer ${EXPLORER_VERSION} source"
-curl -L https://github.com/ergoplatform/explorer-backend/archive/refs/tags/${EXPLORER_VERSION}.tar.gz > explorer-backend-${EXPLORER_VERSION}.tar.gz
+echo "Fetching explorer-backend ${EXPLORER_BRANCH} branch"
+curl -L https://github.com/ergoplatform/explorer-backend/archive/refs/heads/${EXPLORER_BRANCH}.tar.gz > explorer-backend.tar.gz
 
 echo "Extracting explorer source"
-rm -rf explorer-backend/${EXPLORER_VERSION}
-tar -xf explorer-backend-${EXPLORER_VERSION}.tar.gz
-rm explorer-backend-${EXPLORER_VERSION}.tar.gz
+rm -rf explorer-backend-${EXPLORER_BRANCH}
+tar -xf explorer-backend.tar.gz
+rm explorer-backend.tar.gz
 
 echo "Preparing Dockerfiles"
-cp explorer-backend-${EXPLORER_VERSION}/modules/chain-grabber/Dockerfile explorer-backend-${EXPLORER_VERSION}/chain-grabber.Dockerfile
-cp explorer-backend-${EXPLORER_VERSION}/modules/explorer-api/Dockerfile explorer-backend-${EXPLORER_VERSION}/explorer-api.Dockerfile
-cp explorer-backend-${EXPLORER_VERSION}/modules/utx-broadcaster/Dockerfile explorer-backend-${EXPLORER_VERSION}/utx-broadcaster.Dockerfile
-cp explorer-backend-${EXPLORER_VERSION}/modules/utx-tracker/Dockerfile explorer-backend-${EXPLORER_VERSION}/utx-tracker.Dockerfile
+cp explorer-backend-${EXPLORER_BRANCH}/modules/chain-grabber/Dockerfile explorer-backend-${EXPLORER_BRANCH}/chain-grabber.Dockerfile
+cp explorer-backend-${EXPLORER_BRANCH}/modules/explorer-api/Dockerfile explorer-backend-${EXPLORER_BRANCH}/explorer-api.Dockerfile
+cp explorer-backend-${EXPLORER_BRANCH}/modules/utx-broadcaster/Dockerfile explorer-backend-${EXPLORER_BRANCH}/utx-broadcaster.Dockerfile
+cp explorer-backend-${EXPLORER_BRANCH}/modules/utx-tracker/Dockerfile explorer-backend-${EXPLORER_BRANCH}/utx-tracker.Dockerfile
 
 echo "Fixing deprecated OpenJDK base images"
-sed -i.bak 's|FROM openjdk:8-jre-slim|FROM eclipse-temurin:8-jre|g' explorer-backend-${EXPLORER_VERSION}/*.Dockerfile
-rm -f explorer-backend-${EXPLORER_VERSION}/*.Dockerfile.bak
+sed -i.bak 's|FROM openjdk:8-jre-slim|FROM eclipse-temurin:8-jre|g' explorer-backend-${EXPLORER_BRANCH}/*.Dockerfile
+rm -f explorer-backend-${EXPLORER_BRANCH}/*.Dockerfile.bak
 
 echo "Fixing broken SNAPSHOT dependency"
-sed -i.bak 's|v3.3.8-aaaab5ef-SNAPSHOT|3.3.7|g' explorer-backend-${EXPLORER_VERSION}/project/versions.scala
-rm -f explorer-backend-${EXPLORER_VERSION}/project/versions.scala.bak
+sed -i.bak 's|v3.3.8-aaaab5ef-SNAPSHOT|3.3.7|g' explorer-backend-${EXPLORER_BRANCH}/project/versions.scala
+rm -f explorer-backend-${EXPLORER_BRANCH}/project/versions.scala.bak
 
 echo "Done."
